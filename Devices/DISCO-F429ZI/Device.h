529286b63f03812f61fc218bd70c1789eade8a67
dobova <dome@ledonet.it>
2018-04-14 11:49:27 +0200

    Fixes SDRAM F429-Disco and SPI5 pins

--- Devices/DISCO-F429ZI/Device.h
+++ Devices/DISCO-F429ZI/Device.h
@@ -117,10 +117,11 @@
                           /* TIM14 */ { { PIN(A,  7), AF(9)   }, { PIN_NONE  , AF_NONE }, { PIN_NONE  , AF_NONE }, { PIN_NONE  , AF_NONE } },\
                          }
 
-#define INCLUDE_SPI
-#define STM32F4_SPI_SCLK_PINS { { PIN(B, 3), AF(5) }, { PIN(B, 10), AF(5) } }
-#define STM32F4_SPI_MISO_PINS { { PIN(B, 4), AF(5) }, { PIN(C,  2), AF(5) } }
-#define STM32F4_SPI_MOSI_PINS { { PIN(B, 5), AF(5) }, { PIN(C,  3), AF(5) } }
+// NOTE: SPI5 is used for LCD IL9341 and MEMS Sensor
+#define INCLUDE_SPI				// SPI1					SPI2					SPI3					SPI4					SPI5
+#define STM32F4_SPI_SCLK_PINS { { PIN(B, 3), AF(5) }, { PIN(B, 10), AF(5) }, { PIN_NONE  , AF_NONE }, { PIN_NONE  , AF_NONE }, { PIN(F, 7), AF(5) } }
+#define STM32F4_SPI_MISO_PINS { { PIN(B, 4), AF(5) }, { PIN(C,  2), AF(5) }, { PIN_NONE  , AF_NONE }, { PIN_NONE  , AF_NONE }, { PIN(F, 8), AF(5) } }
+#define STM32F4_SPI_MOSI_PINS { { PIN(B, 5), AF(5) }, { PIN(C,  3), AF(5) }, { PIN_NONE  , AF_NONE }, { PIN_NONE  , AF_NONE }, { PIN(F, 9), AF(5) } }
 
 #define INCLUDE_UART
 #define STM32F4_UART_DEFAULT_TX_BUFFER_SIZE  { 256, 256, 256, 256 }
