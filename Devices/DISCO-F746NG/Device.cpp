8ae104a0d130c4ba86c8a1f284b6ceed8244a37b
dobova <dome@ledonet.it>
2019-02-04 15:54:18 +0100

    Final 1.0.0.0_p2

--- Devices/DISCO-F746NG/Device.cpp
+++ Devices/DISCO-F746NG/Device.cpp
@@ -12,12 +12,14 @@ extern "C" {
 
 void STM32F7_Startup_OnSoftResetDevice(const TinyCLR_Api_Manager* apiProvider, const TinyCLR_Interop_Manager* interopManager) {
 	
-	STM32F7_DebugLed(LED_DEBUG, true);	
 	DevicesInterop_Add(interopManager);
 	//STM32F7_Display_Enable((TinyCLR_Display_Controller*)apiProvider);
 
 }
 
 void STM32F7_Startup_MpuConfiguration() {
-	//STM32F7_Mpu_Configuration(reinterpret_cast<uint32_t>(reinterpret_cast<uint32_t*>(&HeapBegin)), STM32F7_Mpu_RegionSize::Size_32MBytes, STM32F7_Mpu_RegionNumber::Region0, true);
+#ifdef USE_SDRAM_HEAP
+		STM32F7_Mpu_Configuration(reinterpret_cast<uint32_t>(reinterpret_cast<uint32_t*>(&HeapBegin)), STM32F7_Mpu_RegionSize::Size_32MBytes, STM32F7_Mpu_RegionNumber::Region0, true);
+#endif
+	STM32F7_DebugLed(LED_DEBUG, true);	
 }
